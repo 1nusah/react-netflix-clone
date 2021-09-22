6e5b5b37286d1de38866050a75826e9da0d48374
Inusah Said <saidinusah29@gmail.com>
2021-09-22 10:25:21 +0000

    empty favelist view

--- src/components/navBar/navbar.js
+++ src/components/navBar/navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
 		// <div className="navBar">
 		<div className={`flex justify-between  ${navBar}`}>
 			<div className="flex space-x-3 justify-center">
-				<div className="w-20 h-10 bg-red-500">
+				<div className="w-20 h-10 ">
 					<img src={Netflix} alt="" className="object-contain" />
 				</div>
 
