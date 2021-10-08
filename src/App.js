afcbd497e8d8372828163022c0406109a0c2c447
Inusah Said <saidinusah29@gmail.com>
2021-10-08 13:22:53 +0000

    improvements to navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 import Banner from './components/banner/banner';
 import Navbar from './components/navBar/navbar';
 import FaveMovieListProvider from './context/movieListContext';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	useParams,
+} from 'react-router-dom';
 import FaveItemsPage from './pages/faveItemsPage/FaveItemsPage';
 import LandingPage from './pages/LandingPage/landingPage';
 function App() {
@@ -19,6 +24,9 @@ function App() {
 							<LandingPage />
 						</Route>
 					</FaveMovieListProvider>
+					<Route path="/movie/:id" exact>
+						<MovieView />
+					</Route>
 				</Switch>
 			</Router>
 		</>
@@ -26,3 +34,9 @@ function App() {
 }
 
 export default App;
+
+const MovieView = () => {
+	const { banner } = useParams();
+	console.log('params', banner);
+	return <div className="text-red-500 text-3xl">hi</div>;
+};
--- src/components/navBar/navbar.js
+++ src/components/navBar/navbar.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './navbar.css';
 import Netflix from '../../assets/netflix_icon.svg';
-import { Avatar, Button } from '@material-ui/core';
+import { Avatar, Button, IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
 	const navBar = 'navBar';
@@ -12,12 +13,21 @@ const Navbar = () => {
 				<div className="w-20 h-10 ">
 					<img src={Netflix} alt="" className="object-contain" />
 				</div>
-
-				{/* <div className=" w-20 h-10 "></div> */}
 			</div>
-			<div className="w-16 h-16 pt-2">
+			<div className="flex justify-center items-center   ">
+				<Link to="/search">
+					<IconButton disableFocusRipple disableRipple>
+						<SearchIcon style={{ color: '#E50914' }} />
+					</IconButton>
+				</Link>
 				<Link to="/favorites">
-					<Button style={{ color: '#E50914', fontWeight: 500 }}>Faves</Button>
+					<Button
+						disableFocusRipple
+						disableRipple
+						style={{ color: '#E50914', fontSize: 13 }}
+					>
+						Faves
+					</Button>
 				</Link>
 			</div>
 		</div>
