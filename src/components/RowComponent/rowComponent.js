f8b9b91beb2723f7ecb57c5dd6a1ad614bdaf606
Inusah Said <saidinusah29@gmail.com>
2021-10-12 14:43:37 +0000

    roruting to move detials

--- src/components/RowComponent/rowComponent.js
+++ src/components/RowComponent/rowComponent.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useContext } from 'react';
 import YouTube from 'react-youtube';
 import movieTrailer from 'movie-trailer';
+import { useHistory } from 'react-router-dom';
 
 import PlayCircleFilledWhiteIcon from '@material-ui/icons/PlayCircleFilledWhite';
 import AddIcon from '@material-ui/icons/Add';
 import ClearOutlinedIcon from '@material-ui/icons/ClearOutlined';
 import FavoriteOutlinedIcon from '@material-ui/icons/FavoriteOutlined';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import { Dialog, IconButton } from '@material-ui/core';
 
 import axios from '../axios/axios';
@@ -66,6 +68,7 @@ const RowComponent = ({ title, fetchUrl, largeRow }) => {
 	const removeFaveItem = (id) => removeMovie(id);
 
 	const index = favemovieList.findIndex((item) => selectedMovie.id === item.id);
+	const history = useHistory();
 	return (
 		<div>
 			<div>
@@ -126,8 +129,13 @@ const RowComponent = ({ title, fetchUrl, largeRow }) => {
 							<IconButton style={{ border: '1px solid #9CA3AF' }}>
 								<PlayCircleFilledWhiteIcon style={{ color: '#fff' }} />
 							</IconButton>
-							<IconButton style={{ border: '1px solid #9CA3AF' }}>
-								<AddIcon className="text-gray-400" />
+							<IconButton
+								onClick={() => {
+									history.push(`/movie/${selectedMovie.id}`, selectedMovie);
+								}}
+								style={{ border: '1px solid #9CA3AF' }}
+							>
+								<InfoOutlinedIcon className="text-gray-400" />
 							</IconButton>
 							{index === -1 ? (
 								<IconButton
