73c7a30efdbb36acc80a08b8c8ecf718b07cf00b
Inusah Said <saidinusah29@gmail.com>
2021-10-16 02:56:33 +0000

    navabr items centered

--- src/components/navBar/navbar.js
+++ src/components/navBar/navbar.js
@@ -8,7 +8,9 @@ const Navbar = () => {
 	const navBar = 'navBar';
 	return (
 		// <div className="navBar">
-		<div className={`flex justify-between bg-black bg-opacity-30  ${navBar}`}>
+		<div
+			className={`flex justify-between items-center bg-black bg-opacity-30  ${navBar}`}
+		>
 			<div className="flex space-x-3 justify-center">
 				<div className="w-20 h-10 ">
 					<Link to="/">
