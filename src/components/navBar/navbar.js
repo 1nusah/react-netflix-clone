8c52325f1eb43b9cdfc39bec5cf2b0ef429d9fc3
Inusah Said <saidinusah29@gmail.com>
2022-01-04 11:04:37 +0000

    navbar gradient

--- src/components/navBar/navbar.js
+++ src/components/navBar/navbar.js
@@ -8,31 +8,33 @@ const Navbar = () => {
 	const navBar = 'navBar';
 	return (
 		// <div className="navBar">
-		<div
-			className={`flex justify-between items-center bg-black bg-opacity-30  ${navBar}`}
-		>
-			<div className="flex space-x-3 justify-center">
-				<div className="w-20 h-10 ">
-					<Link to="/">
-						<img src={Netflix} alt="" className="object-contain" />
+		<div className="h-10v absolute justify-between w-full bg-gradient-to-b from-gray-900 ">
+			<div
+				className={`flex justify-between items-center  bg-gradient-to-b from-bg-black bg-opacity-80 `}
+			>
+				<div className="flex space-x-3 justify-center">
+					<div className="w-20 h-10 ">
+						<Link to="/">
+							<img src={Netflix} alt="" className="object-contain" />
+						</Link>
+					</div>
+				</div>
+				<div className="flex justify-center items-center   ">
+					<Link to="/search">
+						<IconButton disableFocusRipple disableRipple>
+							<SearchIcon style={{ color: '#E50914' }} />
+						</IconButton>
+					</Link>
+					<Link to="/favorites">
+						<Button
+							disableFocusRipple
+							disableRipple
+							style={{ color: '#E50914', fontSize: 13 }}
+						>
+							Faves
+						</Button>
 					</Link>
 				</div>
-			</div>
-			<div className="flex justify-center items-center   ">
-				<Link to="/search">
-					<IconButton disableFocusRipple disableRipple>
-						<SearchIcon style={{ color: '#E50914' }} />
-					</IconButton>
-				</Link>
-				<Link to="/favorites">
-					<Button
-						disableFocusRipple
-						disableRipple
-						style={{ color: '#E50914', fontSize: 13 }}
-					>
-						Faves
-					</Button>
-				</Link>
 			</div>
 		</div>
 		// </div>
